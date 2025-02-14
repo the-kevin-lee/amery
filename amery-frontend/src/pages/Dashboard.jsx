dfb788811ec676b3de47d4bd19356a2c0fe7a7c4
kevin <kl.klee.k.lee@gmail.com>
2025-02-14 13:44:22 -0500

    token auth

--- amery-frontend/src/pages/Dashboard.jsx
+++ amery-frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/");
+        }
+    }, []);
     return (
         <>
         <h1>
